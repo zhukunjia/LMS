61b55e48859b3b893100bf4678ec0aa558aa55be
zhukunjia <njuee111180216@126.com>
2026-05-16 00:48:37 +0800

    优化时间返回格式

--- src/main/java/org/lms/dto/library/LibraryDTO.java
+++ src/main/java/org/lms/dto/library/LibraryDTO.java
@@ -1,5 +1,6 @@
 package org.lms.dto.library;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -23,7 +24,12 @@ public class LibraryDTO {
     private Integer status;
 
     private String createBy;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
+
     private String lastModifiedBy;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date lastModifiedTime;
 }
